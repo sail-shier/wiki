5eff41a9977a641365b625e0e822652a0b95d284
sail <80558411@github.com>
2017-12-01 14:20:46 +0800

    开启博客

---
+++ amWiki/mounts/nav-226.js
@@ -0,0 +1,1 @@
+if(typeof AWPageMounts=='undefined'){AWPageMounts={}};AWPageMounts['nav']={"name":"$navigation.md","path":"$navigation.md","content":"\n#### [首页](?file=home-首页)\n\n##### 储存\n\n##### 主题\n\n##### 测试\n","timestamp":1512109220290}
