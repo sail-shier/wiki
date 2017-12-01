86903100c493bf276f091e2506db85b748253021
sail <80558411@github.com>
2017-12-01 17:49:56 +0800

    更新

--- amWiki/mounts/nav-226.js
+++
@@ -1,1 +0,0 @@
-if(typeof AWPageMounts=='undefined'){AWPageMounts={}};AWPageMounts['nav']={"name":"$navigation.md","path":"$navigation.md","content":"\n#### [首页](?file=home-首页)\n\n##### 储存\n\n##### 主题\n\n##### 测试\n","timestamp":1512109524255}
