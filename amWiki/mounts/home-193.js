5861d849dae91b08b2a518c23176f2094421c176
sail <80558411@github.com>
2017-12-01 18:13:39 +0800

    更改

--- amWiki/mounts/home-193.js
+++
@@ -1,1 +0,0 @@
-if(typeof AWPageMounts=='undefined'){AWPageMounts={}};AWPageMounts['home']={"name":"home-首页.md","path":"home-首页.md","content":"# 个人知识管理\n\n<br>\n\n还没想好写什么，就先骂骂福哥吧！\n","timestamp":1512121761831}
--- amWiki/mounts/nav-230.js
+++ amWiki/mounts/nav-230.js
@@ -1,1 +1,1 @@
-if(typeof AWPageMounts=='undefined'){AWPageMounts={}};AWPageMounts['nav']={"name":"$navigation.md","path":"$navigation.md","content":"\n#### [首页](?file=home-首页)\n\n##### 储存\n- **做事方法论及规范**\n\n##### 主题\n","timestamp":1512121761831}
+if(typeof AWPageMounts=='undefined'){AWPageMounts={}};AWPageMounts['nav']={"name":"$navigation.md","path":"$navigation.md","content":"\n#### [首页](?file=home-首页)\n\n##### 储存\n- **做事方法论及规范**\n\n##### 主题\n","timestamp":1512123212574}
