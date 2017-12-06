d5142edc1b8b7ce088c9c771442a4ab4a366ba94
sail <80558411@github.com>
2017-12-06 17:15:54 +0800

    更新基础方法论

--- amWiki/mounts/home-195.js
+++ amWiki/mounts/home-195.js
@@ -1,1 +1,1 @@
-if(typeof AWPageMounts=='undefined'){AWPageMounts={}};AWPageMounts['home']={"name":"home-首页.md","path":"home-首页.md","content":"# 个人知识管理\n\n<br>\n\n还没想好写什么，就先骂骂福哥吧!  \n","timestamp":1512123334955}
+if(typeof AWPageMounts=='undefined'){AWPageMounts={}};AWPageMounts['home']={"name":"home-首页.md","path":"home-首页.md","content":"# 个人知识管理\n\n<br>\n\n还没想好写什么，就先骂骂福哥吧!  \n","timestamp":1512551736794}
---
+++ amWiki/mounts/nav-289.js
@@ -0,0 +1,1 @@
+if(typeof AWPageMounts=='undefined'){AWPageMounts={}};AWPageMounts['nav']={"name":"$navigation.md","path":"$navigation.md","content":"\n#### [首页](?file=home-首页)\n\n##### 储存\n- **做事方法论及规范**\n    - [基础方法论](?file=1-储存/01-做事方法论及规范/001-基础方法论 \"基础方法论\")\n\n##### 主题\n","timestamp":1512551736794}
